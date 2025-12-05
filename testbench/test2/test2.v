16d3a6dacfd6b28644d36167078711f9be68cad0
VLSI-Shubh <vlsi.shubh@gmail.com>
2025-12-05 01:10:22 -0500

    Update test2.v

--- testbench/test2/test2.v
+++ testbench/test2/test2.v
@@ -14,10 +14,8 @@ module rra_tb_2;
   wire gnt1;
   wire gnt0;
 
-  // Clock: 10 ns period
   always #5 clk = ~clk;
 
-  // One time quantum = 100 ns
   localparam QUANTUM_NS = 100;
 
   initial begin
@@ -31,31 +29,67 @@ module rra_tb_2;
     req2 = 0;
     req3 = 0;
 
- 
     #30 rst = 0;
 
+    // Multiple requests held beyond quantum - should see rotation
+    req0 = 1;
+    req1 = 1;
+    #350;
+    req0 = 0;
+    req1 = 0;
 
+    #20;
 
-    #20  req0 = 1;         
-    #220 req0 = 0;          
+    // Three-way conflict
+    req0 = 1;
+    req1 = 1;
+    req2 = 1;
+    #400;
+    req0 = 0;
+    req1 = 0;
+    req2 = 0;
 
- 
-    #10  req1 = 1;        
-    #240 req1 = 0;          
+    #20;
 
-  
-    #10  req2 = 1;          
-    #260 req2 = 0;         
+    // All requesters competing
+    req0 = 1;
+    req1 = 1;
+    req2 = 1;
+    req3 = 1;
+    #450;
+    req0 = 0;
+    req1 = 0;
+    req2 = 0;
+    req3 = 0;
 
-   
-    #10  req3 = 1;         
-    #220 req3 = 0;          
+    #20;
 
+    // Dynamic pattern - add requests during operation
+    req0 = 1;
+    req2 = 1;
+    #150;
+    req1 = 1;
+    req3 = 1;
+    #250;
+    req0 = 0;
+    #150;
+    req1 = 0;
+    req2 = 0;
+    req3 = 0;
+
+    #20;
+
+    // Preemption test - req0 held long, req1 added later
+    req0 = 1;
+    #50;
+    req1 = 1;
+    #250;
+    req0 = 0;
+    req1 = 0;
 
     #100 $finish;
   end
 
-  // DUT
   rra U (
     .clk (clk),
     .rst (rst),
