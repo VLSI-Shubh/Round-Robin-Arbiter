29ae1513e0707ec799002e3df65ced7c8292ea46
VLSI-Shubh <vlsi.shubh@gmail.com>
2025-12-05 01:26:21 -0500

    Update run_sim.sh

--- run_sim.sh
+++ run_sim.sh
@@ -2,20 +2,28 @@
 
 set -e
 
+RTL="src/rra.v"
 
-RTL="src/rra.v"         
-TB="testbench/test1/test1.v"         
-SIM_EXE="sim.out"    
+TB1="testbench/test1/test1.v"
+TB2="testbench/test2/test2.v"
+
+SIM_EXE1="sim_test1.out"
+SIM_EXE2="sim_test2.out"
 
 SYNTH_SCRIPT="synth/synth_rra.ys"
 SHOW_SCRIPT="synth/show_rra.ys"
 
+echo "[SIM1] Compiling testbench 1 + RTL..."
+iverilog -g2012 -o "$SIM_EXE1" "$RTL" "$TB1"
 
-echo "[SIM] Compiling testbench + RTL..."
-iverilog -g2012 -o "$SIM_EXE" "$RTL" "$TB"
+echo "[SIM1] Running simulation 1..."
+vvp "$SIM_EXE1"
 
-echo "[SIM] Running simulation..."
-vvp "$SIM_EXE"
+echo "[SIM2] Compiling testbench 2 + RTL..."
+iverilog -g2012 -o "$SIM_EXE2" "$RTL" "$TB2"
+
+echo "[SIM2] Running simulation 2..."
+vvp "$SIM_EXE2"
 
 echo "[SYNTH] Running Yosys synthesis..."
 yosys -s "$SYNTH_SCRIPT"
